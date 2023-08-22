9fd93d63fc73e6fd6f8b5b52c43ff8d5bf63637e
Milton8086 <chidukspty@gmail.com>
2023-08-22 10:23:54 -0700

    update

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    for count, d in enumerate(my_list):
-        if d % 2 == 0:
-            boolist[count] = True
-        else:
-            boolist[count] = False
-    return(boolist)
+def delete_at(my_list=[], idx=0):
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+    return my_list
