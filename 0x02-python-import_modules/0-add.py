20a1699863a1c68f8a0860802a06b7c4f53b775e
Milton8086 <chidukspty@gmail.com>
2023-08-24 06:43:06 -0700

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
-if __name__ == "__main":
-    """Print the sum of 1 and 2."""
+if __name__ == "__main__":
     from add_0 import add
 
-    b = 2
     a = 1
-    print("{} + {} = {}".format(a,b,add(a,b)))
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+from variable_load_5 import a
+
+
+def find_var():
+    print(a)
+
 
 if __name__ == "__main__":
-"""mports the variable a from the file variable_load_5.py and prints its value.""""
-
-print(a)
+    find_var()
